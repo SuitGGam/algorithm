f30e96854fe046e1e03409c1bfad5375c1c09ddb
donghyeon <gmj49@naver.com>
2025-08-28 00:11:09 +0900

    refactor: 역방향 그래프를 만들어서 다익스트라 호출 횟수 최적화

--- baekjoon-all/01000+/BOJ1238.java
+++ baekjoon-all/01000+/BOJ1238.java
@@ -13,6 +13,13 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class BOJ1238 {
     // 입력을 위한 객체 선언
     static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +27,7 @@ public class BOJ1238 {
     
     static int N, X; // 학생의 수, 파티를 여는 마을의 번호
     static ArrayList<Node>[] village; // 마을 도로에 대한 정보를 담을 배열
+    static ArrayList<Node>[] reverseVillage; // 마을 도로에 대한 정보를 반대로 담을 배열
     
     static class Node { // 도로 정보를 담을 Node
         int E; // 다른 마을로 가는 도로
@@ -34,18 +42,13 @@ public class BOJ1238 {
     static int[] time;       // 총 소요 시간을 담을 배열
     static int[] goXTime;    // X로 가는 소요 시간을 담을 배열
     static int[] goHomeTime; // X에서 집에 오는 소요 시간을 담을 배열
-    static void reset() { // X로 갔다 오는 소요 시간을 초기화 하는 함수
-        for (int i = 1; i <= N; i++) {
-            goXTime[i]    = Integer.MAX_VALUE;
-        }
-    } // reset 종료
     
-    static void goX(int start) { // X로 가는 시간 구하는 함수
+    static void goX() { // X로 가는 시간 구하는 함수
         PriorityQueue<Node> minHeap = new PriorityQueue<>((o1, o2) ->
                 Integer.compare(o1.T, o2.T)); // 최솟값을 저장할 PQ
         
-        goXTime[start] = 0;
-        minHeap.add(new Node(start, 0));
+        goXTime[X] = 0;
+        minHeap.add(new Node(X, 0));
         
         while (!minHeap.isEmpty()) {
             Node curNode = minHeap.poll(); // 현재 마을
@@ -53,8 +56,8 @@ public class BOJ1238 {
             // 현재 노드의 시간이 기존에 기록된 것보다 크다면 continue
             if (goXTime[curNode.E] < curNode.T) continue;
             
-            for (int i = 0; i < village[curNode.E].size(); i++) {
-                Node nxtNode = village[curNode.E].get(i); // 다음 마을
+            for (int i = 0; i < reverseVillage[curNode.E].size(); i++) {
+                Node nxtNode = reverseVillage[curNode.E].get(i); // 다음 마을
                 
                 if (goXTime[nxtNode.E] > goXTime[curNode.E] + nxtNode.T) {
                     goXTime[nxtNode.E] = goXTime[curNode.E] + nxtNode.T; // 소요 시간 갱신
@@ -65,14 +68,10 @@ public class BOJ1238 {
         }
         
         // 시간 합산하기
-        time[start] += goXTime[X];
+        for (int i = 1; i <= N; i++) time[i] += goXTime[i];
     } // goX 종료
     
     static void goHome() { // X에서 집으로 오는 시간 구하는 함수
-        for (int i = 1; i <= N; i++) { // 소요 시간 초기화
-            goHomeTime[i] = Integer.MAX_VALUE;
-        }
-        
         PriorityQueue<Node> minHeap = new PriorityQueue<>((o1, o2) ->
                 Integer.compare(o1.T, o2.T)); // 최솟값을 저장할 PQ
         
@@ -106,8 +105,12 @@ public class BOJ1238 {
         int M = Integer.parseInt(st.nextToken()); // 단방향 도로의 개수
         X = Integer.parseInt(st.nextToken()); // 파티를 여는 마을의 번호
         
-        village = new ArrayList[N + 1]; // 배열 크기 지정
-        for (int i = 1; i <= N; i++) village[i] = new ArrayList<>();
+        village        = new ArrayList[N + 1]; // 배열 크기 지정
+        reverseVillage = new ArrayList[N + 1]; // 배열 크기 지정
+        for (int i = 1; i <= N; i++) { // 배열 생성
+            village[i]        = new ArrayList<>();
+            reverseVillage[i] = new ArrayList<>();
+        }
         
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -116,6 +119,7 @@ public class BOJ1238 {
             int T = Integer.parseInt(st.nextToken()); // 소요 시간
             
             village[V].add(new Node(E, T)); // 도로 정보 추가
+            reverseVillage[E].add(new Node(V, T)); // 도로 정보 추가
         }
         
         // 버퍼 닫기
@@ -125,13 +129,12 @@ public class BOJ1238 {
         goXTime    = new int[N + 1]; // 배열 크기 지정
         goHomeTime = new int[N + 1]; // 배열 크기 지정
         
-        for (int i = 1; i <= N; i++) { // 각 마을에서 X로 출발
-            if (i == X) continue; // X 출발하는 경우는 continue
-            
-            reset(); // 시작 전 시간 초기화
-            goX(i);  // X로 출발
+        for (int i = 1; i <= N; i++) { // 소요 시간 초기화
+            goXTime[i]    = Integer.MAX_VALUE;
+            goHomeTime[i] = Integer.MAX_VALUE;
         }
         
+        goX();    // X로 출발
         goHome(); // X에서 출발
         
         int maxTime = 0; // 최대 소요 시간
